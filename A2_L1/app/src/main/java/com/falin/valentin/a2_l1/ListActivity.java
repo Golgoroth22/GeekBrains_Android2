1cf882015f057e2c23c569b22f1e4284723074e6
Golgoroth22 <falinv22@gmail.com>
2018-02-22 15:17:11 +0700

    Add functional to check temperature by input city name

--- A2_L1/app/src/main/java/com/falin/valentin/a2_l1/ListActivity.java
+++ A2_L1/app/src/main/java/com/falin/valentin/a2_l1/ListActivity.java
@@ -2,7 +2,9 @@ package com.falin.valentin.a2_l1;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.design.widget.FloatingActionButton;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,15 +14,31 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.falin.valentin.a2_l1.data.FakeDB;
+import com.falin.valentin.a2_l1.data.WeatherDataLoader;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class ListActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+    private static final String LOG_TAG = ListActivity.class.getSimpleName();
     private MyListViewAdapter adapter;
     private TextView contentListViewText;
+    private EditText contentCityInputEditText;
+    private ImageButton contentCityApplyButton;
+    private TextView contentDegreesTextView;
+
+    private static String city = "";
+    private static String cityDegrees = "";
+
+    private final Handler handler = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,17 +62,66 @@ public class ListActivity extends AppCompatActivity
         toggle.syncState();
 
         setListView();
+
         contentListViewText = findViewById(R.id.content_list_view_text);
-        checkNoteBookSize();
+        contentCityApplyButton = findViewById(R.id.content_check_city_button);
+        contentCityApplyButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                city = contentCityInputEditText.getText().toString();
+                updateWeatherData();
+            }
+        });
+        contentCityInputEditText = findViewById(R.id.content_city_edit_text);
+        contentCityInputEditText.setText(city);
+        contentDegreesTextView = findViewById(R.id.content_degrees_text_view);
+        contentDegreesTextView.setText(cityDegrees);
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        checkNoteBookSize();
     }
 
     private void setListView() {
         ListView listView = findViewById(R.id.content_list_view);
         adapter = new MyListViewAdapter(this);
         listView.setAdapter(adapter);
+    }
+
+    private void updateWeatherData() {
+        new Thread() {
+            @Override
+            public void run() {
+                final JSONObject jsonObject = WeatherDataLoader.getJSONData(getApplicationContext(), city);
+                if (jsonObject == null) {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            Toast.makeText(getApplicationContext(), "City not found", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                } else {
+                    handler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            renderWeather(jsonObject);
+                        }
+                    });
+                }
+            }
+        }.start();
+    }
+
+    private void renderWeather(JSONObject jsonObject) {
+        try {
+            JSONObject mainObject = jsonObject.getJSONObject("main");
+            cityDegrees = String.format("%.2f", mainObject.getDouble("temp")) + " ℃";
+            contentDegreesTextView.setText(cityDegrees);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            Log.d(LOG_TAG, "One or more fields not found in the JSON data");
+        }
     }
 
     @Override
@@ -88,10 +155,10 @@ public class ListActivity extends AppCompatActivity
                 checkNoteBookSize();
                 return true;
             }
-            case R.id.action_add: {
-                adapter.addElement();
-                return true;
-            }
+//            case R.id.action_add: {
+//                adapter.addElement();
+//                return true;
+//            }
             default: {
                 return false;
             }
